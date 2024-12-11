a1c52e6a8c0718d4bf5abf12abf8b84e239e594b
ThuanSon <thuanhtml@gmail.com>
2024-12-11 12:58:10 +0700

    db->connect->reconnect

--- database/db.js
+++ database/db.js
@@ -1,20 +1,33 @@
 const mysql = require("mysql2");
 require('dotenv').config();
-// MySQL connection configuration
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
 
-// Connect to MySQL
-db.connect((err) => {
-    if (err) {
-        console.error("Error connecting to MySQL:", err.stack);
-        return;
-    }
-    console.log("Connected to MySQL database np");
-});
+let db;
+
+function connectToDatabase() {
+    db = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+
+    db.connect((err) => {
+        if (err) {
+            console.error("Error connecting to MySQL:", err.stack);
+            setTimeout(connectToDatabase, 5000); // Retry after 5 seconds
+        } else {
+            console.log("Connected to MySQL database np");
+        }
+    });
+
+    db.on('error', (err) => {
+        console.error('MySQL error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            connectToDatabase(); // Reconnect if connection is lost
+        }
+    });
+}
+
+connectToDatabase();
 
 module.exports = db;
